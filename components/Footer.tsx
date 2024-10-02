774659e493accea1d72d7a3a7113c289aceb4ec9
oj-bomi <eileen@oj.vision>
2024-10-02 16:31:14 +0900

    푸터 글씨 사이즈 수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,13 +7,18 @@ export default function Footer() {
 
 <div className="col text-center text-md-start bg-black py-[50px]">
    
-   <p className="mb-0 text-small text-white py-[5px]">
-     오제이 주식회사(OJ Inc.)<span className="middot">/</span>블레이볼(blayball)<span className="middot">/</span>CEO 오지완<span className="middot">/</span>CBO 강영모<span className="middot">/</span> Business Registration No141-81-49244
+   <p className="mb-0 text-white py-[5px] text-[10px] sm:text-[1vw]">
+     오제이 주식회사(OJ Inc.)<span className="middot">/</span>블레이볼(blayball)
     </p>
-   <p className="mb-0 text-small text-white py-[5px]">
+
+    <p className="mb-0 text-white py-[5px] text-[10px] sm:text-[1vw]">
+    CEO 오지완<span className="middot">/</span>CBO 강영모<span className="middot">/</span> Business Registration No141-81-49244
+    </p>
+
+   <p className="mb-0 text-white py-[5px] text-[10px] sm:text-[1vw]">
     경기도 파주시 회동길 37-14<span className="middot">/</span>TEL. 031-947-0792<span className="middot">/</span>Email : info@oj.vision
    </p>
-   <p className="mb-0 text-small text-white py-[5px]">
+   <p className="mb-0 text-white py-[5px] text-[10px] sm:text-[1vw]">
      Copyrights © 2024 All Rights Reserved by OJ Inc.
    </p>
 
